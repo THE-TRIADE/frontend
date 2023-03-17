ddc03f10303c852d4081b9ded3d2e535a85ab6ef
Lucas Nogueira <lucas.nogueira.072@ufrn.edu.br>
2023-03-17 15:49:35 -0300

    Remove comentario

--- src/exemplo1/Aula1.jsx
+++ src/exemplo1/Aula1.jsx
@@ -2,9 +2,6 @@ import { useState } from 'react';
 
 export const Aula1 = () => {
 	let contador = 0;
-	// Returns a stateful value, and a function to update it.
-	// useState<number>(initialState: number | (() => number)):
-	// [number, React.Dispatch<React.SetStateAction<number>>]
 	const [count, setCount] = useState(0);
 
 	const incrementCount = () => {
