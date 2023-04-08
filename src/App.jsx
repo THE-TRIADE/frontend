7fcce17c77a8c6dc1bb4fe2cef8a0593a04a11bb
ana.mgholandadev <ana.mgholandadev@gmail.com>
2023-04-08 18:26:24 -0300

    Adiciona rotas do projeto
    Co-authored-by: Lucas Vinícius <fawnbr@users.noreply.github.com>
    Co-authored-by: Leonardo David <leodavidbr@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,21 @@
-import { useEffect, useState } from 'react';
-import { Button } from './components/Button';
-import { PasswordLoginInput } from './components/LoginInputs/PasswordLoginInput';
-import { TextualLoginInput } from './components/LoginInputs/TextualLoginInput';
-import { SelectLoginInput } from './components/LoginInputs/SelectLoginInput';
-import { CustomLink } from './components/CustomLink';
-import { TextualInput } from './components/Inputs/TextualInput';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { SignUp } from './pages/SignUp';
+import { SignIn } from './pages/SignIn';
+import { ForgotPassword } from './pages/ForgotPassword';
+import { NotFound } from './pages/NotFound';
 
 function App() {
-	const [signUpForm, setSignUpForm] = useState({
-		name: '',
-		email: '',
-		cpf: '',
-		birthDate: '',
-		password: '',
-	});
-
-	const [option, setOption] = useState('A');
-
-	useEffect(() => {
-		console.log(signUpForm);
-	}, [signUpForm]);
-
-	const updateForm = (inputName, event) => {
-		setSignUpForm((prevState) => {
-			return { ...prevState, [inputName]: event.target.value };
-		});
-	};
-
 	return (
-		<div className="app">
-			<h2>Cadastro</h2>
-			<p>
-				Cadastre-se e tenha acesso ao <b>Family</b>Routine
-			</p>
-			<TextualLoginInput placeholder="Nome" onChange={(e) => updateForm('name', e)} size="large" />
-			<TextualLoginInput placeholder="E-mail" onChange={(e) => updateForm('email', e)} size="large" />
-			<TextualLoginInput placeholder="CPF" onChange={(e) => updateForm('cpf', e)} size="large" />
-			<TextualLoginInput placeholder="Data de Nascimento" onChange={(e) => updateForm('birthDate', e)} size="large" />
-			<PasswordLoginInput placeholder="Senha" onChange={(e) => updateForm('password', e)} size="large" />
-			<PasswordLoginInput
-				placeholder="Confirmar Senha"
-				onChange={(e) => console.log('Confirmar senha: ' + e.target.value)}
-				size="large"
-			/>
-			<SelectLoginInput
-				options={['A', 'B', 'C']}
-				value={option}
-				onChange={(e) => setOption(e.target.value)}
-				size="large"
-			/>
-			<CustomLink href="www.google.com" text="Voltar para página inicial" target="_blank" />
-			<TextualInput label="Email" placeholder="E-mail" onChange={(e) => updateForm('email', e)} size="large" />
-			<h4>Dependente 1</h4>
-			<Button onClick={() => console.log('Cliquei')} text="Cadastrar" size="large" />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/signup" element={<SignUp />} />
+				<Route path="/login" element={<SignIn />} />
+				<Route path="/forgotpassword" element={<ForgotPassword />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
