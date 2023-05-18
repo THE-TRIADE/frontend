4d62260a7e9253f4a690aa88d567a0138aff8fcd
ana.mgholandadev <ana.mgholandadev@gmail.com>
2023-05-17 23:59:35 -0300

    Detalhamento de grupo familiar

--- src/pages/FamilyGroupDetails.jsx
+++ src/pages/FamilyGroupDetails.jsx
@@ -5,13 +5,31 @@ import { useEffect } from 'react';
 import { FamilyGroupDetail } from '../components/FamilyGroupDetail';
 import { useParams } from 'react-router-dom';
 
+const getActivities = (dependentId) => {
+	return api.get('/activity', { params: { dependentId } }).then((res) => {
+		let late = 0,
+			created = 0,
+			inProgress = 0;
+		res.data.forEach((activity) => {
+			console.log(activity);
+			if (activity.dependentId === dependentId) {
+				if (activity.state === 'LATE') {
+					late++;
+				} else if (activity.state === 'CREATED') {
+					created++;
+				} else if (activity.state === 'IN_PROGRESS') {
+					inProgress++;
+				}
+			}
+		});
+		return { late, created, inProgress };
+	});
+};
+
 export const FamilyGroupDetails = () => {
 	const { id } = useParams();
 	const [familyGroup, setFamilyGroup] = useState(null);
-
-	const [activitiesLate, setActivitiesLate] = useState(0);
-	const [activitiesCreated, setActivitiesCreated] = useState(0);
-	const [activitiesInProgress, setActivitiesInProgress] = useState(0);
+	const [activities, setActivities] = useState({});
 
 	useEffect(() => {
 		const getFamilyGroup = () => {
@@ -19,31 +37,18 @@ export const FamilyGroupDetails = () => {
 				setFamilyGroup(res.data);
 			});
 		};
-		const getActivities = (idDependent) => {
-			let late = 0,
-				created = 0,
-				in_progress = 0;
-			api.get('/activity').then((res) => {
-				res.data.forEach((activity) => {
-					console.log(activity);
-					if (activity.dependentId === idDependent) {
-						if (activity.state === 'LATE') {
-							late++;
-						} else if (activity.state === 'CREATED') {
-							created++;
-						} else if (activity.state === 'IN_PROGRESS') {
-							in_progress++;
-						}
-					}
-					setActivitiesLate(late);
-					setActivitiesCreated(created);
-					setActivitiesInProgress(in_progress);
+		getFamilyGroup();
+	}, [id]);
+
+	useEffect(() => {
+		if (familyGroup) {
+			familyGroup.dependents.forEach((dependent) => {
+				getActivities(dependent.id).then((activity) => {
+					setActivities((a) => ({ ...a, [dependent.id]: activity }));
 				});
 			});
-		};
-		getFamilyGroup();
-		getActivities(1);
-	}, []);
+		}
+	}, [familyGroup]);
 
 	return (
 		<div className="app">
@@ -56,9 +61,9 @@ export const FamilyGroupDetails = () => {
 							<CardDependents
 								key={dependent.id}
 								dependent={dependent}
-								late={activitiesLate}
-								created={activitiesCreated}
-								in_progress={activitiesInProgress}
+								late={activities[dependent.id]?.late ?? 0}
+								created={activities[dependent.id]?.created ?? 0}
+								in_progress={activities[dependent.id]?.inProgress ?? 0}
 							/>
 						))}
 				</div>
